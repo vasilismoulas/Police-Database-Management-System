259aecdc926a8e5950d68f2621064415fc4a8c52
Vasilis Moulas <vasilismoulas11@gmail.com>
2024-07-20 10:06:48 +0300

    Implement Police Database

---
+++ CriminalDatabase/PoliceCriminalDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Police_Database_Management_System.CriminalDatabase
+{
+    public class PoliceCriminalDatabase : ICriminalGroup, ICriminalRecord
+    {
+        public List<CriminalRecord> CriminalRecords { get; private set; }
+        public List<CriminalGroup> CriminalGroups { get; private set; }
+
+
+        public PoliceCriminalDatabase()
+        {
+
+        }
+        public PoliceCriminalDatabase(List<CriminalRecord> CriminalRecords, List<CriminalGroup> CriminalGroups)
+        {
+            this.CriminalRecords = CriminalRecords;
+            this.CriminalGroups = CriminalGroups;
+        }
+
+
+        // Methods to modify records and groups
+        public void AddCriminalRecord(CriminalRecord member)
+        {
+            if (member == null) throw new ArgumentNullException();
+            CriminalRecords.Add(member);
+        }
+
+        public void RemoveCriminalRecord(CriminalRecord member)
+        {
+            member = CriminalRecords.Contains(member) ? member : throw new ArgumentNullException();
+            CriminalRecords.Remove(member);
+        }
+
+        //public void ChangeMember(CriminalGroup group, CriminalRecord member)
+        //{
+        //    CriminalGroup selected_group = CriminalGroups.Contains(group) ? group : null;
+        //}
+
+        public ReadOnlyCollection<CriminalRecord> getCriminalRecords()
+        {
+            return new ReadOnlyCollection<CriminalRecord>(CriminalRecords);
+        }
+
+        public ReadOnlyCollection<CriminalGroup> getCriminalGroups()
+        {
+            return new ReadOnlyCollection<CriminalGroup>(CriminalGroups);
+        }
+
+    }
+}
---
+++ CriminalDatabase/Read-Write_Access/ICriminalGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Police_Database_Management_System.CriminalDatabase
+{
+    public interface ICriminalGroup
+    {
+        public ReadOnlyCollection<CriminalGroup> getCriminalGroups();
+    }
+}
